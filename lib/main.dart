ca4eb8538fe24a71533aadce1b2cdd7a91a3647d
DESKTOP-UQMVDN2\myr_r <myr_ryusuke@hotmail.co.jp>
2024-02-12 23:30:44 +0900

    :sparkles: feat: ベースをalarmライブラリのexampleコードに変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
 
 import 'package:alarm/alarm.dart';
+import 'package:alarming/scenes/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,97 +11,10 @@ Future<void> main() async {
 
   await Alarm.init(showDebugLogs: true);
 
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: const MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late List<AlarmSettings> alarms;
-  static StreamSubscription<AlarmSettings>? subscription;
-
-  @override
-  void initState() {
-    super.initState();
-    if (Alarm.android) {
-      checkAndroidNotificationPermission();
-    }
-
-    loadAlarms();
-    subscription ??= Alarm.ringStream.stream.listen(
-      (alarmSettings) => const Placeholder(),
-      onDone: () {
-        print('subscription:onDone');
-      },
-    );
-  }
-
-  void loadAlarms() {
-    setState(() {
-      alarms = Alarm.getAlarms();
-      alarms.sort((a, b) => a.dateTime.isBefore(b.dateTime) ? 0 : 1);
-      print(alarms.length);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // var appState = context.watch<MyAppState>();
-
-    return Scaffold(
-      body: Column(
-        children: alarms
-            .map((alarm) => Text(alarm.hashCode.toRadixString(16)))
-            .toList(),
-      ),
-    );
-  }
-
-  Future<void> checkAndroidNotificationPermission() async {
-    final status = await Permission.notification.status;
-    if (status.isDenied) {
-      alarmPrint('Requesting notification permission...');
-      final res = await Permission.notification.request();
-      alarmPrint(
-        'Notification permission ${res.isGranted ? '' : 'not'} granted.',
-      );
-    }
-  }
-
-  Future<void> checkAndroidExternalStoragePermission() async {
-    final status = await Permission.storage.status;
-    if (status.isDenied) {
-      alarmPrint('Requesting external storage permission...');
-      final res = await Permission.storage.request();
-      alarmPrint(
-        'External storage permission ${res.isGranted ? '' : 'not'} granted.',
-      );
-    }
-  }
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: false),
+      home: const ExampleAlarmHomeScreen(),
+    ),
+  );
 }
